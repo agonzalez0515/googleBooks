adeb9f50d7e885f1219a7478895ad6ee0a1b6f90
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-06-16 17:45:08 -0700

    Clean up class names and add enter key handler

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import styles from './search-form.module.css'
+import styles from './search-form.module.scss'
 
 const SearchForm = ({
+  handleInput,
   handleSubmit,
+  keyPress,
   searchTerm,
-  handleInput
 }) => {
   return ( 
-    <div className={styles.containerSearch}>
-      <form className={styles.formSearch}>
+    <div className={styles.container}>
+      <form className={styles.form}>
         <label htmlFor='search-term'>Enter search term: </label>
         <div className={styles.containerInput}>
-          <input className={styles.formInput} type='text' id='search-term' value={searchTerm} onChange={handleInput}/>
-          <button className={styles.formSubmit} type='button' onClick={handleSubmit}>Submit</button>
+          <input className={styles.input} type='text' id='search-term' value={searchTerm} onChange={handleInput} onKeyDown={keyPress} />
+          <button className={styles.submit} type='button' onClick={handleSubmit}>Submit</button>
         </div>
       </form>
     </div> 
