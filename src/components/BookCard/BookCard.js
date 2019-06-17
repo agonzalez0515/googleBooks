248f43f85d7925f7238dedf5dc8c3c4916d77539
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-06-16 17:46:01 -0700

    Clean up classes

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import styles from './book-card.module.css'
+import styles from './book-card.module.scss'
 
 const BookCard = ({details}) => {
-  const { id, volumeInfo: {title, subtitle, authors,publisher, imageLinks,publishedDate} } = details
+  const { volumeInfo: {title, subtitle, authors,publisher, imageLinks,publishedDate} } = details
   return (
-    <div key={id} className={styles.card}>
+    <div className={styles.card}>
       <div className={`${styles.cardImage} ${styles.grow}`} style={{backgroundImage: `url(${imageLinks.thumbnail})`}}></div>
       <h2 className={styles.cardTitle}>{title}</h2>
       {subtitle && <h3 className={styles.carSubtitle}>{subtitle}</h3>}
