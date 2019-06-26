34f51a48ba5ed91f985c42905504227c5e887e52
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-06-25 22:44:56 -0700

    Handle no results and no book cover

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { authorsNames } from '../../helpers'
+import { ReactComponent as Link } from '../../assets/link.svg'
+import noCover from '../../assets/noCover.svg'
 import styles from './book-card.module.scss'
 
 const BookCard = ({details}) => {
-  const { volumeInfo: { title, subtitle, authors, publisher, imageLinks, infoLink }} = details
+  const { volumeInfo: { title, subtitle, authors, publisher, imageLinks = {}, infoLink }} = details
   return (
     <div className={styles.card}>
-      <div className={`${styles.cardImage} ${styles.grow}`} style={{backgroundImage: `url(${imageLinks.thumbnail})`}}></div>
+      <div className={`${styles.cardImage} ${styles.grow}`} style={{backgroundImage: `url(${imageLinks.thumbnail || noCover })`}}></div>
       <h2 className={styles.cardTitle}>{title}</h2>
-      {subtitle && <h3 className={styles.carSubtitle}>{subtitle}</h3>}
-      <p>by {authorsNames(authors)}</p>
-
+      {subtitle && <h3 className={styles.cardSubtitle}>{subtitle}</h3>}
+      {authors && <p className={styles.authors}>by {authorsNames(authors)}</p>}
       {publisher && <p className={styles.cardPublisher}>{publisher}</p>}
-      <p><a href={infoLink} target="_blank" rel="noopener noreferrer">More information</a></p>
+      <p><Link className={styles.linkIcon} /><a href={infoLink} target="_blank" rel="noopener noreferrer">More information</a></p>
     </div>
   )
 }
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React from 'react'
+import noResults from '../../assets/noResults.svg'
 import BookCard from '../BookCard/BookCard'
 
 
 const BookList = ({books}) => {
-    return books.map(book => {
+  console.log(books.totalItems)
+  if(books.totalItems > 0) {
+    return books.items.map(book => {
       return <BookCard 
         key={book.id} 
         details={book}
       />
     })
+  }
+  else {
+    return (
+      <div>
+        <img src={noResults} className='sitting' alt='Empty clipboards' />
+        <p style={{textAlign: 'center', fontSize: '1.3rem'}}>No books found. Try again!</p>
+      </div>
+    )
+  }
 }
  
-export default BookList;
+export default BookList
+
+// TODO: Refactor no search results into component
