9da0e6e7c4c2f82291d6a82f5efd11246576f6fa
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-06-25 22:46:22 -0700

    Update class name

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -16,7 +16,7 @@ const BookList = ({books}) => {
   else {
     return (
       <div>
-        <img src={noResults} className='sitting' alt='Empty clipboards' />
+        <img src={noResults} className='placeholderImg' alt='Empty clipboards' />
         <p style={{textAlign: 'center', fontSize: '1.3rem'}}>No books found. Try again!</p>
       </div>
     )
