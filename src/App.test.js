0b4986ddfbc5e2c82eec03171453736d7220e1f1
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-07-06 17:14:08 -0700

    Finish App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
 import App from './App'
+import { mockResponse } from './helpers'
+import { fetchBooks } from './fetchBooks'
+
+jest.mock('./fetchBooks.js')
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
@@ -9,13 +13,36 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div)
 })
 
-// it('fetches books', () => {
+it('fetches books when button is clicked', async () => {
+  fetchBooks.mockImplementationOnce(() => Promise.resolve(mockResponse))
+  const { getByText } = render(<App />)
+  fireEvent.click(getByText('Submit'))
+  const resolvedBookTitle = await waitForElement(()=> getByText('test') )
 
-//   const {getByText} = render(<App />)
-//   const button = getByText('Submit')
-//   fireEvent.click(button)
+  expect(fetchBooks).toHaveBeenCalled()
+  expect(resolvedBookTitle).toBeInTheDocument()
+})  
 
-//   expect()
-// })
+it('fetches books when enter key is pressed', async () => {
+  fetchBooks.mockImplementationOnce(() => Promise.resolve(mockResponse))
+  const { getByText, container } = render(<App />)
+  fireEvent.keyDown(container.getElementsByTagName('input')[0], {
+    key: 'Enter',
+    keyCode: 13
+  })
+  const resolvedBookTitle = await waitForElement(()=> getByText('test') )
 
-// TODO: finish tests
+  expect(fetchBooks).toHaveBeenCalled()
+  expect(resolvedBookTitle).toBeInTheDocument()
+})
+
+it('does not fetch books when other keys are pressed', async () => {
+  fetchBooks.mockImplementationOnce(() => Promise.resolve(mockResponse))
+  const { getByAltText, container } = render(<App />)
+  fireEvent.keyDown(container.getElementsByTagName('input')[0], {
+    key: 'Shift',
+    keyCode: 16
+  })
+  
+  expect(getByAltText('Girl reading and sitting on stack of books')).toBeInTheDocument()
+})
