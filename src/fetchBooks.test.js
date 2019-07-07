67f65e5cd8248affa41f08056a7241b5989e6e72
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-07-06 17:28:20 -0700

    Add fetch module test

---
+++ src/fetchBooks.test.js
@@ -0,0 +1,19 @@
+
+import { fetchBooks } from './fetchBooks'
+ 
+describe('testing api call', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+ 
+  it('calls google books and returns data to me', () => {
+    fetch.mockResponseOnce(JSON.stringify({items: []}))
+ 
+    fetchBooks('harry potter').then(res => {
+      expect(res.items).toEqual([])
+    })
+  
+    expect(fetch.mock.calls.length).toEqual(1)
+    expect(fetch.mock.calls[0][0]).toEqual('https://www.googleapis.com/books/v1/volumes?q=harry potter&key=AIzaSyAPFGh3tQZET-EnMos5-Lw9bpfJjzg9pe4')
+  })
+})
