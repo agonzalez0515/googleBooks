a8b810b7623770c394ae4e8872cd23952ee44fcd
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-07-07 21:54:54 -0700

    Organize book card sections

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { authorsNames } from '../../helpers'
-import { ReactComponent as Link } from '../../assets/link.svg'
 import noCover from '../../assets/noCover.svg'
 import styles from './book-card.module.scss'
 
@@ -8,12 +7,21 @@ const BookCard = ({details}) => {
   const { volumeInfo: { title, subtitle, authors, publisher, imageLinks = {}, infoLink }} = details
   return (
     <div className={styles.card}>
-      <div className={`${styles.cardImage} ${styles.grow}`} style={{backgroundImage: `url(${imageLinks.thumbnail || noCover })`}}></div>
-      <h2 className={styles.cardTitle}>{title}</h2>
-      {subtitle && <h3 className={styles.cardSubtitle}>{subtitle}</h3>}
-      {authors && <p className={styles.authors}>by {authorsNames(authors)}</p>}
-      {publisher && <p className={styles.cardPublisher}>{publisher}</p>}
-      <p className={styles.link}><Link className={styles.linkIcon} /><a href={infoLink} target="_blank" rel="noopener noreferrer">More information</a></p>
+      <a href={infoLink} target="_blank" rel="noopener noreferrer">
+        <div className={`${styles.cardImage} ${styles.grow}`} style={{backgroundImage: `url(${imageLinks.thumbnail || noCover })`}}>
+          <span className={styles.link} >Click for more information</span>
+        </div>
+      </a>
+      <div className={styles.cardInfo}>
+        <div className={styles.cardTitles}>
+          <h2 className={styles.cardTitle}>{title}</h2>
+          {subtitle && <h3 className={styles.cardSubtitle}>{subtitle}</h3>}
+          {authors && <p className={styles.authors}>by {authorsNames(authors)}</p>}
+        </div>
+        <div>
+          {publisher && <p className={styles.cardPublisher}>{publisher}</p>}
+        </div>
+      </div>
     </div>
   )
 }
